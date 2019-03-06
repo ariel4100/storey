68e323d04bfec9f6d3a919b2235b85308a88d226
ariel4100 <arielcallisaya00@gmail.com>
2019-03-06 17:09:38 -0300

    model mc

---
+++ database/migrations/2019_03_06_132311_create_servicios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServiciosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo')->nullable();
+            $table->text('descripcion')->nullable();
+            $table->string('file_image')->nullable();
+            $table->string('orden')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicios');
+    }
+}
